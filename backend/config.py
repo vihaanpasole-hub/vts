8e8a8a8a22ef004b0960a74672a0a0dcd373e85f
vihaanpasole-hub <vihaanpasole@gmail.com>
2026-01-12 12:14:19 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,7 @@ from backend.models import db
 
 class Config:
     SECRET_KEY = "vts_secret_123"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///../instance/database.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/database.db"
+
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
